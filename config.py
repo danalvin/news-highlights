dd181467c2e4e343fcaf29829607b52f7cf3d58d
danalvin <mungaiwaituika@gmail.com>
2018-08-31 15:33:23 +0300

    classes added ton the file

---
+++ config.py
@@ -0,0 +1,34 @@
+import os
+
+class Config:
+    """docstring for .
+    """
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/{}?apiKey={}'
+    ARTICLE_API_BASE_URL = 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=16fa6a565fc14afe8bef13b530dc2e47'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+
+class ProdConfig(Config):
+    """
+    docstring for .
+    """
+    pass
+    # def __init__(self, arg):
+    #     super(, self).__init__()
+    #     self.arg = arg
+
+class DevConfig(Config):
+    """docstring for .
+    """
+
+    DEBUG = True
+    # def __init__(self, arg):
+    #     super(, self).__init__()
+    #     self.arg = arg
+
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig
+}
